8bd0e8b1648e65b4fd59663376a3443caf000612
bigbull88 <luubinhhn@gmail.com>
2021-12-06 18:48:07 +0700

    new

---
+++ exp game/Assets/AllCode/Scorer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        hits = hits + 1;
+        Debug.Log("Bumped thing many times:" + hits); 
+       
+    }
+}
