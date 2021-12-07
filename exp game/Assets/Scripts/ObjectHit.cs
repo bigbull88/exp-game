82b208c527374f84b1b6ee568e75e9164fb1e711
bigbull88 <luubinhhn@gmail.com>
2021-12-07 15:32:54 +0700

    update ObjectHit.cs

--- exp game/Assets/Scripts/ObjectHit.cs
+++ exp game/Assets/Scripts/ObjectHit.cs
@@ -10,7 +10,7 @@ public class ObjectHit : MonoBehaviour
         if(other.gameObject.tag == "Player")
            {
                GetComponent<MeshRenderer>().material.color = Color.red; 
-               gameObject.tag == "Hit";
+               gameObject.tag = "Hit";
             }
     }
 }
